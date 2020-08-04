d71cb58954e48a3d0b9182e8b4a99c3b7df3df84
k7krishnar <45996242+k7krishnar@users.noreply.github.com>
2020-08-04 21:39:05 +0530

    test commit

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,12 @@
 #test
+import string
+
+def check_palindrome(string1):
+    print("input: " + string1)
+    stripped1 = [ s for s in string1.lower() if s.isalpha()]
+    rev = stripped1[::-1]
+    return rev == stripped1
+
+
+string1 = """Go Hang a Salami! I'm a Lasagna Ho!"""
+print(check_palindrome(string1))
